dc112eff723e3461681732793ab38ed6c20bde37
Ben De Meester <ben.demeester@ugent.be>
2014-04-17 11:32:49 +0200

    startAgent should be a separate thread

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-sh ./ServerAgent-2.2.1/startAgent.sh
+sh ./ServerAgent-2.2.1/startAgent.sh &
 python getmetrics.py output/metric_server_virtuoso.csv cpu:name=fuseki--network:bytessent--network:bytesrecv honegger &
 apache-jmeter-2.11/bin/jmeter.sh -n -t "LDF eval.jmx"
 python stopmetrics.py output/metric_server_virtuoso.csv
