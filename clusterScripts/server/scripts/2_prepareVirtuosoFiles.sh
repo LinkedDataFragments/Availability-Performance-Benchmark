d21a19aa519e9dc7b2740d4f223deb8c751fc5c5
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2014-04-24 10:49:36 +0200

    Uncommented important things :)

--- clusterScripts/server/scripts/2_prepareVirtuosoFiles.sh
+++ clusterScripts/server/scripts/2_prepareVirtuosoFiles.sh
@@ -3,7 +3,7 @@
 echo 'Extracting virtuoso db files from S3. This can take a while...'
 
 # extract Virtuoso data files to instance drive 1
-#echo '=== preparing files on drive 1 ==='
+echo '=== preparing files on drive 1 ==='
 s3cmd get s3://ldf/dataset/bsbm/100M/virtuoso_drive1.tar.xz /mnt/drive2 &&\
 tar xvJf /mnt/drive2/virtuoso_drive1.tar.xz -C /mnt/drive1 &&\
 rm /mnt/drive2/virtuoso_drive1.tar.xz
