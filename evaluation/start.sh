858e27e7e16d6474a21e754a30c138812028f1c2
Ben De Meester <ben.demeester@ugent.be>
2014-04-30 10:55:33 +0200

    do own metrics collection

--- evaluation/start.sh
+++ evaluation/start.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-./ServerAgent-2.2.1/startAgent.sh --auto-shutdown &
-python getmetrics.py "../output/metric_server_virtuoso.csv" "cpu:name=virtuoso--network:bytessent--network:bytesrecv" "ec2-54-86-22-42.compute-1.amazonaws.com" &
-./apache-jmeter-2.11/bin/jmeter -n -t "./LDF eval2.jmx"
-python stopmetrics.py "../output/metric_server_virtuoso.csv"
-python parseclient.py "../output/metric_client_*.csv"
-python parsebsbm.py "../output/out_*.txt"
+BACKENDPROGRAM="virtuoso"
+python getmetrics.py "../output/metric_server_$BACKENDPROGRAM.csv" "10.0.0.244" &
+./apache-jmeter-2.11/bin/jmeter -n -r -t "./LDF eval2.jmx"
+python stopmetrics.py "../output/metric_server_$BACKENDPROGRAM.csv"
+python archiveoutput.py
