496da8f45564a2a3ea21fbccc7ffc5cb2b5c4eee
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2014-05-12 13:41:59 +0200

    Skip unnecessary splitting of data files to be read by Virtuoso, since Virtuoso 7 loads data multithreaded out of the box.

--- clusterScripts/server/scripts/init_1_prepareVirtuosoFiles_bsbm.sh
+++ clusterScripts/server/scripts/init_1_prepareVirtuosoFiles_bsbm.sh
@@ -5,10 +5,8 @@ echo 'Retrieving dataset file from S3...'
 s3cmd get s3://ldf/dataset/bsbm/100M/dataset.nt.xz /mnt/drive2 &&\
 mkdir -p /mnt/drive1/dataset/bsbm/100M &&\
 cd /mnt/drive1/dataset/bsbm/100M &&\
-echo 'Splitting dataset into smaller parts...' &&\
-xzcat /mnt/drive2/dataset.nt.xz | split -l 2000000 --additional-suffix=.nt - dataset_ &&\
+echo 'Converting dataset to gzipped file...' &&\
+xzcat /mnt/drive2/dataset.nt.xz | pigz -9 > dataset.nt.gz &&\
 rm /mnt/drive2/dataset.nt.xz &&\
-echo 'gzipping parts...' &&\
-pigz -v -9 dataset* &&\
 echo 'Done!'
 
